a0f228d4c43cf64c939c46330af6bceac16a9f34
Francesco Lanza <35008288+fralnz@users.noreply.github.com>
2024-10-24 12:09:49 +0200

    Add files via upload

--- src/main/java/org/spring1/ufs15/dao/Evento.java
+++ src/main/java/org/spring1/ufs15/dao/Evento.java
@@ -1,4 +1,41 @@
-package org.spring1.ufs15.dao;
+package org.spring1.ufs15.model;
 
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Eventi")
 public class Evento {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    @Size(min=4, max=25)
+    String nome;
+
+    @NotNull
+    @Size(min=1)
+    String descrizione;
+
+    @NotNull
+    @Size(min=1, max=25)
+    String organizzatore;
+
+    @NotNull
+    Date dataInizio;
+
+    @NotNull
+    Date dataFine;
+
+    @NotNull
+    Integer idStanza;
+
+    public Evento() {
+    }
 }
