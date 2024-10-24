b02241a143aa6e2f8562196f0fbc3bfa5bd587f6
banica84 <giulia.banica@itsrizzoli.it>
2024-10-24 12:25:30 +0200

    Lanza non carino con me e mazzi

---
+++ src/main/java/org/spring1/ufs15/dao/EventoDao.java
@@ -0,0 +1,22 @@
+package org.spring1.ufs15.dao;
+
+import org.spring1.ufs15.model.Evento;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
+public interface EventoDao extends CrudRepository<Evento> {
+    List<Evento> findByTitolo(String titolo);
+    List<Evento> findByOrganizzatore(String organizzatore);
+    List<Evento> findByMostraEvento(int mostraEvento);
+    List<Evento> findByDescrizione(String descrizione);
+    List<Evento> findByDataIn(Date dataIn);
+    List<Evento> findByDataFin(Date dataFin);
+    List<Evento> findByEtaMin(int etaMin);
+    List<Evento> findByLimitePers(int limitePers);
+    List<Evento> findByStanza(String stanza);
+
+    Evento findByID(long id);
+
+}
