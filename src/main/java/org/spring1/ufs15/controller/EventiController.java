95df1e3f5182cd3690cc97364277d9673aabd104
FrancescoLanza <francesco@fragiolanza.eu>
2024-10-28 12:10:08 +0100

    migliorato eventi controller

--- src/main/java/org/spring1/ufs15/controller/EventiController.java
+++ src/main/java/org/spring1/ufs15/controller/EventiController.java
@@ -5,6 +5,7 @@ import org.spring1.ufs15.model.Evento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,7 +13,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 @Controller
-@RequestMapping(path = "/evento")
+@RequestMapping(path = "/eventi")
 public class EventiController {
 
     @Autowired
@@ -36,5 +37,10 @@ public class EventiController {
         model.addAttribute("evento", evento);
         
         return "CreazioneEvento";
+    }
+    @RequestMapping("/creaEvento/{id}")
+    public String remove(@PathVariable("id") long id) {
+        eventoRepository.deleteById(id);
+        return "redirect:/eventi/";
     }
 }
--- src/main/java/org/spring1/ufs15/dao/EventoDao.java
+++ src/main/java/org/spring1/ufs15/dao/EventoDao.java
@@ -1,26 +1,82 @@
 package org.spring1.ufs15.dao;
 
 import org.spring1.ufs15.model.Evento;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
 public interface EventoDao extends CrudRepository<Evento, Long> {
+    default <S extends Evento> S save(S entity) {
+        return null;
+    }
+
+    default <S extends Evento> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    default Optional<Evento> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    default boolean existsById(Long aLong) {
+        return false;
+    }
+
+    default Iterable<Evento> findAll() {
+        return null;
+    }
+
+    default Iterable<Evento> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    default long count() {
+        return 0;
+    }
+
+    default void deleteById(Long aLong) {
+
+    }
+
+    default void delete(Evento entity) {
+
+    }
+
+    default void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    default void deleteAll(Iterable<? extends Evento> entities) {
+
+    }
+
+    default void deleteAll() {
+
+    }
+
     List<Evento> findByTitolo(String titolo);
+
     List<Evento> findByOrganizzatore(String organizzatore);
+
     List<Evento> findByIdTipo(int idTipo);
+
     List<Evento> findByDescrizione(String descrizione);
+
     List<Evento> findByDataInizio(Date dataInizio);
+
     List<Evento> findByDataFine(Date dataFine);
+
     List<Evento> findByEtaMinima(int etaMinima);
+
     List<Evento> findByLimitePersone(int limitePersone);
+
     List<Evento> findByIdStanza(int idStanza);
 
     Evento findById(long id);
 
-    @Query("SELECT * FROM eventi")
-    Evento checkCredentials(@Param("email") String email, @Param("password") String password);
-
+/*    @Query("SELECT a FROM Evento a WHERE a.id = :id")
+    Evento checkInfo(@Param("titolo") String titolo, @Param("organizzatore") String organizzatore, @Param("descrizione") String descrizione, @Param("dataInizio") Date dataInizio, @Param("dataFine") Date dataFine);
+*/
 }
