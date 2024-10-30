ba0c976c03c4873ceaae901173be00bfb6940e79
FrancescoLanza <francesco@fragiolanza.eu>
2024-10-30 15:39:18 +0100

    query automatica degli eventi

--- src/main/java/org/spring1/ufs15/config/Config.java
+++ src/main/java/org/spring1/ufs15/config/Config.java
@@ -1,7 +1,9 @@
 package org.spring1.ufs15.config;
 
 import org.spring1.ufs15.dao.AdminDao;
+import org.spring1.ufs15.dao.EventoDao;
 import org.spring1.ufs15.model.Admin;
+import org.spring1.ufs15.model.Evento;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,16 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
 
     @Bean
-    CommandLineRunner commandLineRunner(AdminDao adminRepo) {
+    CommandLineRunner adminStarter(AdminDao adminRepo) {
         return args -> {
             adminRepo.save(new Admin(1, "pippo@pera.it", "puppa", "Pippo", "Pera"));
+        };
+    }
+
+    @Bean
+    CommandLineRunner eventiStarter(EventoDao eventiRepo) {
+        return args -> {
+            eventiRepo.save(new Evento(1, "Van Gogh", "Mostra assurda", "Amsterdam", null, null, 104, 8, 2, 1));
         };
     }
 }
