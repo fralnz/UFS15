9155c720b33594fedb9bca64382a3a0e8434b411
banica84 <giulia.banica@itsrizzoli.it>
2024-10-30 14:22:00 +0100

    Aggiunto CSS alle pagine HTML

--- src/main/java/org/spring1/ufs15/controller/AdminController.java
+++ src/main/java/org/spring1/ufs15/controller/AdminController.java
@@ -2,7 +2,6 @@ package org.spring1.ufs15.controller;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-import org.spring1.ufs15.model.Evento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
