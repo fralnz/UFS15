0e3e1d022646b889602399a046dccb923689a2c2
FrancescoLanza <francesco@fragiolanza.eu>
2024-10-30 14:06:26 +0100

    EventoDao ritorna una lista di Evento

--- src/main/java/org/spring1/ufs15/dao/EventoDao.java
+++ src/main/java/org/spring1/ufs15/dao/EventoDao.java
@@ -1,6 +1,7 @@
 package org.spring1.ufs15.dao;
 
 import org.spring1.ufs15.model.Evento;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
@@ -8,6 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EventoDao extends CrudRepository<Evento, Long> {
+
+    @Query("SELECT e FROM Evento e")
+    public List<Evento> getEventiList();
 
     default <S extends Evento> Iterable<S> saveAll(Iterable<S> entities) {
         return null;
