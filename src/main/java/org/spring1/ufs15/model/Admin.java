8a9f5d2e079ed95847faff5c0ac82983c4f8d8c6
fralnz <francesco@fragiolanza.eu>
2024-11-03 20:49:45 +0100

    pattern per il controllo della mail

--- src/main/java/org/spring1/ufs15/model/Admin.java
+++ src/main/java/org/spring1/ufs15/model/Admin.java
@@ -16,6 +16,9 @@ public class Admin {
 
     @NotNull
     @Size(min = 1, max = 25)
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
+            message = "La mail non e' valida: esempio@mail.it")
     String mail;
 
     @NotNull
